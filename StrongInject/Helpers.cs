4b1d6242c70a9b3ad06a39c8d51d730bd3d6f6d8
yair halberstadt <yairhalberstadt@gmail.com>
2020-07-21 07:33:35 +0300

    Check for IAsyncDisposable in NetStandard2.0 as well

--- StrongInject/Helpers.cs
+++ StrongInject/Helpers.cs
@@ -7,12 +7,11 @@ namespace StrongInject
     {
         public static ValueTask DisposeAsync<T>(T instance)
         {
-#if !NETSTANDARD2_0
             if (instance is IAsyncDisposable asyncDisposable)
             {
                 return asyncDisposable.DisposeAsync();
             }
-#endif
+
             if (instance is IDisposable disposable)
             {
                 disposable.Dispose();
