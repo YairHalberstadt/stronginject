0f35ea6c7c4912e37aca93d1b40f2356018e34f4
Yair Halberstadt <yairhalberstadt@gmail.com>
2020-10-22 09:54:45 +0300

    Add initial package providing easy addition of a container to a service collection (#71)
    This is extremely early preview and liable to change or be removed completely at any time

---
+++ StrongInject.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace StrongInject.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddContainerForTransientService<TContainer, TService>(this IServiceCollection services) where TContainer : class, IContainer<TService> where TService : class
+        {
+            services.TryAddSingleton<TContainer, TContainer>();
+            services.AddTransient(x => x.GetRequiredService<TContainer>().Resolve());
+            services.AddTransient(x => x.GetRequiredService<Owned<TService>>().Value);
+        }
+
+        public static void AddContainerForTransientService<TService>(this IServiceCollection services, IContainer<TService> container) where TService : class
+        {
+            services.AddTransient(x => container.Resolve());
+            services.AddTransient(x => x.GetRequiredService<Owned<TService>>().Value);
+        }
+
+        public static void AddContainerForScopedService<TContainer, TService>(this IServiceCollection services) where TContainer : class, IContainer<TService> where TService : class
+        {
+            services.TryAddSingleton<TContainer, TContainer>();
+            services.AddScoped(x => x.GetRequiredService<TContainer>().Resolve());
+            services.AddScoped(x => x.GetRequiredService<Owned<TService>>().Value);
+        }
+
+        public static void AddContainerForScopedService<TService>(this IServiceCollection services, IContainer<TService> container) where TService : class
+        {
+            services.AddScoped(x => container.Resolve());
+            services.AddScoped(x => x.GetRequiredService<Owned<TService>>().Value);
+        }
+
+        public static void AddContainerForSingletonService<TContainer, TService>(this IServiceCollection services) where TContainer : class, IContainer<TService> where TService : class
+        {
+            services.TryAddSingleton<TContainer, TContainer>();
+            services.AddSingleton(x => x.GetRequiredService<TContainer>().Resolve());
+            services.AddSingleton(x => x.GetRequiredService<Owned<TService>>().Value);
+        }
+
+        public static void AddContainerForSingletonService<TService>(this IServiceCollection services, IContainer<TService> container) where TService : class
+        {
+            services.AddSingleton(x => container.Resolve());
+            services.AddSingleton(x => x.GetRequiredService<Owned<TService>>().Value);
+        }
+
+        public static void AddScopedContainerForTransientService<TContainer, TService>(this IServiceCollection services) where TContainer : class, IContainer<TService> where TService : class
+        {
+            services.TryAddScoped<TContainer, TContainer>();
+            services.AddTransient(x => x.GetRequiredService<TContainer>().Resolve());
+            services.AddTransient(x => x.GetRequiredService<Owned<TService>>().Value);
+        }
+
+        public static void AddScopedContainerForScopedService<TContainer, TService>(this IServiceCollection services) where TContainer : class, IContainer<TService> where TService : class
+        {
+            services.TryAddScoped<TContainer, TContainer>();
+            services.AddScoped(x => x.GetRequiredService<TContainer>().Resolve());
+            services.AddScoped(x => x.GetRequiredService<Owned<TService>>().Value);
+        }
+    }
+}
