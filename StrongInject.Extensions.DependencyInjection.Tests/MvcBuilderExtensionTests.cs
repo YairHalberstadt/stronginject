546aeaf221473d956916446aeadbc74c14b36454
yair halberstadt <yairhalberstadt@gmail.com>
2020-11-23 08:43:25 +0200

    Add ResolveControllersThroughServiceProvider. Add ReplaceService APIs and rename existing Add APIs for consistency

---
+++ StrongInject.Extensions.DependencyInjection.Tests/MvcBuilderExtensionTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StrongInject.Extensions.DependencyInjection.Tests
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Controller1 : ControllerBase
+    {
+        [HttpGet] public string Get() => "Controller 1!";
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class Controller2 : ControllerBase
+    {
+        [HttpGet] public string Get() => "Controller 2!";
+    }
+
+    public class MvcBuilderExtensionTests
+    {
+        public class Startup
+        {
+            public Startup(IConfiguration configuration)
+            {
+                Configuration = configuration;
+            }
+
+            public IConfiguration Configuration { get; }
+
+            public void ConfigureServices(IServiceCollection services)
+            {
+                services.AddControllers().ResolveControllersThroughServiceProvider();
+            }
+
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            {
+                app.UseRouting();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+            }
+        }
+
+        [Fact]
+        public async Task ResolveControllersThroughServiceProvider_SuceedsIfRequestedControllerIsRegistered1()
+        {
+            var hostBuilder = new WebHostBuilder()
+                .ConfigureServices(x => x.AddTransient<Controller1>())
+                .UseStartup<Startup>();
+            var server = new TestServer(hostBuilder);
+            Assert.Equal("Controller 1!", await server.CreateClient().GetStringAsync("/controller1"));
+        }
+
+        [Fact]
+        public async Task ResolveControllersThroughServiceProvider_SuceedsIfRequestedControllerIsRegistered2()
+        {
+            var hostBuilder = new WebHostBuilder()
+                .ConfigureServices(x => x.AddTransient<Controller2>())
+                .UseStartup<Startup>();
+            var server = new TestServer(hostBuilder);
+            Assert.Equal("Controller 2!", await server.CreateClient().GetStringAsync("/controller2"));
+        }
+
+        [Fact]
+        public async Task ResolveControllersThroughServiceProvider_ErrorIfRequestedControllerIsNotRegistered()
+        {
+            var hostBuilder = new WebHostBuilder()
+                .ConfigureServices(x => x.AddTransient<Controller1>())
+                .UseStartup<Startup>();
+            var server = new TestServer(hostBuilder);
+            var ex = await Assert.ThrowsAsync<InvalidOperationException>(() => server.CreateClient().GetStringAsync("/controller2"));
+            Assert.Equal("No service for type 'StrongInject.Extensions.DependencyInjection.Tests.Controller2' has been registered.", ex.Message);
+        }
+    }
+}
