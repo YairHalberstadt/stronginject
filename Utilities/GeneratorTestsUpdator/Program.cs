57b08070c63f6f5bac42480ef4a9db575ce90f49
Yair Halberstadt <yairhalberstadt@gmail.com>
2020-11-26 20:39:01 +0200

    Allow resolving a SingleInstance method from a non async lambda, if any surrounding lambda/method is async by resolving it eagerly. (#84)

--- Utilities/GeneratorTestsUpdator/Program.cs
+++ Utilities/GeneratorTestsUpdator/Program.cs
@@ -35,34 +35,43 @@ namespace GeneratorTestsUpdator
         {
             private readonly string _targetFilePath;
             private string _source;
+            private readonly string _originalSource;
             public TestSourceUpdater()
             {
                 var currentDirectory = Directory.GetCurrentDirectory();
                 _targetFilePath = Directory.GetCurrentDirectory()[..(currentDirectory.LastIndexOf("bin"))] + "../../StrongInject.Tests.Unit/GeneratorTests.cs";
-                _source = File.ReadAllText(_targetFilePath);
+                _source = _originalSource = File.ReadAllText(_targetFilePath);
             }
 
             public override bool OnMessageWithTypes(IMessageSinkMessage message, HashSet<string> messageTypes)
             {
-                var correctCodeRegex = new Regex(@"with a length of \d*, but\s*("".*"")\s* has a length of \d*", RegexOptions.Singleline);
+                var correctCodeRegex = new Regex(@", but\s*("".*"")\s* ((has a length of \d*)|(differs near ""))", RegexOptions.Singleline);
                 var originalCodeRegex = new Regex(@"""#pragma.*?(?<!"")""(?!"")", RegexOptions.Singleline);
-                if (message is ITestFailed testFailed)
+                var stackTraceRegex = new Regex(@"StrongInject.Generator.Tests.Unit.GeneratorTests.* (\d+)", RegexOptions.Singleline);
+                if (message is ITestFailed { StackTraces: var stackTraces } testFailed)
                 {
                     var match = correctCodeRegex.Match(testFailed.Messages.FirstOrDefault() ?? "");
-                    if (match.Groups.Count == 2)
+                    if (match.Groups.Count >= 2)
                     {
                         var correctCode = match.Groups[1].Value;
                         correctCode = "\"" + correctCode[1..^1].Replace("\"", "\"\"") + "\"";
                         correctCode = Regex.Replace(correctCode, @"\r\n|\n\r|\n|\r", Environment.NewLine);
+
+                        var stackTrace = stackTraces[0];
+                        var line = int.Parse(stackTraceRegex.Match(stackTrace).Groups[1].Value);
+                        var originalMethodLocation = _originalSource.IndexOf(testFailed.TestMethod.Method.Name);
+                        var part = _originalSource[originalMethodLocation..(_originalSource.IndexOfNth('\n', line + 1))];
+                        var count = originalCodeRegex.Matches(part).Count;
                         var methodLocation = _source.IndexOf(testFailed.TestMethod.Method.Name);
                         var sourceFromMethod = _source[methodLocation..];
-                        var originalCodeMatch = originalCodeRegex.Match(sourceFromMethod);
-                        if (!originalCodeMatch.Success)
+                        var originalCodeMatches = originalCodeRegex.Matches(sourceFromMethod);
+                        if (originalCodeMatches.Count == 0)
                         {
                             Console.WriteLine($"Cannot fix test {testFailed.TestMethod.Method.Name} as can't find string beginning '#pragma'");
                         }
                         else
                         {
+                            var originalCodeMatch = originalCodeMatches[count];
                             _source = _source[..(methodLocation + originalCodeMatch.Index)] + correctCode + sourceFromMethod[(originalCodeMatch.Index + originalCodeMatch.Length)..];
                             Console.WriteLine($"Fixed test {testFailed.TestMethod.Method.Name}");
                         }
@@ -75,6 +84,26 @@ namespace GeneratorTestsUpdator
             {
                 File.WriteAllText(_targetFilePath, _source);
             }
+        }
+    }
+
+    public static class Extensions
+    {
+        public static int IndexOfNth(this string input, char value, int nth)
+        {
+            if (nth < 1)
+                throw new NotSupportedException("Param 'nth' must be greater than 0!");
+
+            var index = 0;
+
+            for (int i = 1; i <= nth; i++)
+            {
+                index = input.IndexOf(value, startIndex: index + 1);
+                if (index == -1)
+                    return -1;
+            }
+
+            return index;
         }
     }
 }
