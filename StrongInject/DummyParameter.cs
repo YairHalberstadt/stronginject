4f49c2359de69b730662528f7a45616117af869b
yair halberstadt <yairhalberstadt@gmail.com>
2020-11-26 12:10:18 +0200

    Add overloads of RunAsync such that:
    `Func<T, ValueTask>` is preferred to `Func<T, Task>` is preferred to `Func<T, TResult>`

---
+++ StrongInject/DummyParameter.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace StrongInject.Internal
+{
+    /// <summary>
+    /// A class with no possible value other than null. Used to mark an optional parameter which should never be set.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public class DummyParameter
+    {
+        private DummyParameter() { }
+    }
+}
+
