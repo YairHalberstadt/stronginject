3e34e9f88f4d76214f7d20e9b72e94bdc1b17cb2
Yair Halberstadt <yairhalberstadt@gmail.com>
2021-01-13 21:05:50 +0200

    Add wpf sample project (#82)
    * Remove wpf from Debug and Release configurations, and add Wpf and All configurations
    
    * Update UsersController.cs
    
    * add windows-build

---
+++ Samples/StrongInject.Samples.Wpf/Commands/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace StrongInject.Samples.Wpf.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object?> _command;
+        private readonly Predicate<object?>? _canExecute;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object?> command) : this(command, null)
+        {
+        }
+
+        public RelayCommand(Action<object?> command, Predicate<object?>? canExecute)
+        {
+            _command = command ?? throw new ArgumentNullException(nameof(command));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _command.Invoke(parameter);
+        }
+    }
+}
