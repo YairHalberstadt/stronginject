4c1436790b819b9f23d28e0d7a4d19ea94e7b1b3
yairh <yairh@google.com>
2021-10-31 08:23:52 +0200

    Add generic RegisterAttributes

--- StrongInject/RegisterAttribute.cs
+++ StrongInject/RegisterAttribute.cs
@@ -42,4 +42,40 @@ namespace StrongInject
         public Type[] RegisterAs { get; }
         public Scope Scope { get; }
     }
+    
+    /// <summary>
+    /// Use this attribute to register <typeparamref name="T"/> with StrongInject.
+    /// The type must have either exactly one public constructor, or exactly two public constructors, one of which is parameterless.
+    /// The type will be registered as an instance of itself.
+    /// </summary>
+    /// <typeparam name="T">The type to register</typeparam>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+    public sealed class RegisterAttribute<T> : Attribute
+    {
+        /// <param name="scope">The scope of each instance of <typeparamref name="T"/> - i.e. how often will a new instance be created.</param>
+        public RegisterAttribute(Scope scope = Scope.InstancePerResolution)
+        {
+            Scope = scope;
+        }
+        
+        public Scope Scope { get; }
+    }
+    
+    /// <summary>
+    /// Use this attribute to register <typeparamref name="TImpl"/> with StrongInject as an instance of <typeparamref name="TService"/>.
+    /// The type must have either exactly one public constructor, or exactly two public constructors, one of which is parameterless.
+    /// </summary>
+    /// <typeparam name="TImpl">The type to register</typeparam>
+    /// <typeparam name="TService">The type to register <typeparamref name="TImpl"/> as an instance of</typeparam>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+    public sealed class RegisterAttribute<TImpl, TService> : Attribute where TImpl : TService
+    {
+        /// <param name="scope">The scope of each instance of <typeparamref name="TImpl"/> - i.e. how often will a new instance be created.</param>
+        public RegisterAttribute(Scope scope = Scope.InstancePerResolution)
+        {
+            Scope = scope;
+        }
+        
+        public Scope Scope { get; }
+    }
 }
